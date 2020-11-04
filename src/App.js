314a423c907964dfff86195d10fc5f6b0a69af34
Peter Ihimire <peterihimire@gmail.com>
2020-11-04 15:29:39 +0100

    Started working on a new project as I was constantly getting error on the previous project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import HomeAbout from "./components/HomeAbout";
+import BuyCar from "./components/BuyCar";
+// import CarOption from "./components/CarOption";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Banner />
+        <HomeAbout />
+        <BuyCar />
+        {/* <CarOption /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
