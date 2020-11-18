70b46069a7d5b56c021f4e450f384fd7987143d1
Peter Ihimire <peterihimire@gmail.com>
2020-11-18 22:35:18 +0100

    Added image for the media and created media component

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Media.css";
+import image from "../assets/Screenshot.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -36,13 +38,77 @@ const Media = (props) => {
     slidesToScroll: 1,
   };
   return (
-    <section>
+    <section className="media">
       <div className="container">
+        <h4 className="media-heading center">media</h4>
         <div className="row">
           <div className="col s12">
             <div className="col s12 hidden-xs">
-              <Slider ref={(c) => (this.slider = c)} {...settings}>
-                <div className=" col s12"></div>
+              <Slider {...settings}>
+                <div className=" col s12">
+                  <div class="col s12 m4">
+                    <div class="video-container vid-frame hidden">
+                      <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/qN_AA2FzweI?rel=0&amp;controls=0&amp;showinfo=0"
+                        frameborder="0"
+                        allow="autoplay; encrypted-media"
+                        allowfullscreen
+                      ></iframe>
+                    </div>
+                    <div class="vid-img">
+                      <img
+                        src={image}
+                        alt="video 1"
+                        style={{ width: "100%", margin: "0 1rem" }}
+                        class="scale-up*"
+                      />
+                    </div>
+                  </div>
+
+                  <div class="col s12 m4">
+                    <div class="video-container vid-frame hidden ">
+                      <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/oDKQYqkmySA?rel=0&amp;controls=0&amp;showinfo=0"
+                        frameborder="0"
+                        allow="autoplay; encrypted-media"
+                        allowfullscreen
+                      ></iframe>
+                    </div>
+                    <div class="vid-img">
+                      <img
+                        src={image}
+                        alt="video 3"
+                        style={{ width: "100%", margin: "0 1rem" }}
+                        class="scale-up*"
+                      />
+                    </div>
+                  </div>
+
+                  <div class="col s12 m4">
+                    <div class="video-container vid-frame hidden ">
+                      <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/oDKQYqkmySA?rel=0&amp;controls=0&amp;showinfo=0"
+                        frameborder="0"
+                        allow="autoplay; encrypted-media"
+                        allowfullscreen
+                      ></iframe>
+                    </div>
+                    <div class="vid-img">
+                      <img
+                        src={image}
+                        alt="video 3"
+                        style={{ width: "100%", margin: "0 1rem" }}
+                        class="scale-up*"
+                      />
+                    </div>
+                  </div>
+                </div>
               </Slider>
             </div>
           </div>
