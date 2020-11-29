d0c180e2daaf00dca8126e36fa794ee6bb697b03
Peter Ihimire <peterihimire@gmail.com>
2020-11-29 20:23:45 +0100

    Started working on the footer, added the footer-logo and the horizontal line

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "./Footer.css";
+import logoAlt from "../assets/logo-alt.svg";
 
 const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <h2>this is the footer component</h2>
+        <div className="row">
+          <div className="col s12">
+            <div className="footer-logo center">
+              <a href="/">
+                <img src={logoAlt} alt="paymonthly logo-alt" />
+              </a>
+            </div>
+            <hr></hr>
+          </div>
+        </div>
       </div>
     </footer>
   );
