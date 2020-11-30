122d7c5e833bfc7e33c218a49a789acd971b2eff
Peter Ihimire <peterihimire@gmail.com>
2020-11-30 15:49:21 +0100

    Footer content taking shape

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,8 +13,8 @@ const Footer = () => {
         </div>
         <hr />
         <div className="row">
-          <div className=" col s12 m4 l4 footer-content-one">
-            <div className="s12 m4">
+          <div className=" col s12 m12 l3 footer-content-one">
+            <div className="s12 m4  white-text">
               <h6>join our mailing list</h6>
               <form>
                 <input />
@@ -22,18 +22,34 @@ const Footer = () => {
               </form>
             </div>
           </div>
-          <div className=" col s12 m8 l8 ">
-            <div className=" col s6 m2">
+          <div className=" col s12 m12 l8 offset-l1 ">
+            <div className=" col s6 m3  white-text">
               <h6>who we are</h6>
+              <ul>
+                <li>about</li>
+                <li>sponsor</li>
+              </ul>
             </div>
-            <div className=" col s6 m2">
+            <div className=" col s6 m3  white-text">
               <h6>our services</h6>
+              <ul>
+                <li>buy a car</li>
+                <li>sell a car</li>
+              </ul>
             </div>
-            <div className=" col s6 m2">
+            <div className=" col s6 m3  white-text">
               <h6>contact us</h6>
+              <ul>
+                <li>+234 806 0958 134</li>
+                <li>peterihimire@gmail.com</li>
+              </ul>
             </div>
-            <div className=" col s6 m2">
+            <div className=" col s6 m3  white-text">
               <h6>support</h6>
+              <ul>
+                <li>FAQs</li>
+                <li>terms and conditions</li>
+              </ul>
             </div>
           </div>
         </div>
