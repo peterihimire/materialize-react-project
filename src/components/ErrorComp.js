5e626c33d4fad5b8c6905e9c15aee268a5387d93
Peter Ihimire <peterihimire@gmail.com>
2020-12-04 10:31:22 +0100

    Fixed the page styling for error component , looking better now with tablet and phone responsiveness

--- src/components/ErrorComp.js
+++ src/components/ErrorComp.js
@@ -1,22 +1,25 @@
 import React from "react";
+import "./ErrorComp.css";
 
 const Error = (props) => {
   console.log(props);
   console.log(props.hestory);
   return (
-    <section>
+    <section className="error-comp">
       <div className="container">
-        <div className="row">
-          <div className="col s12">
-            <h1>404</h1>
-            <h2>page not found</h2>
-            <h3>
-              The requested URL{" "}
-              <span className="notfound">
-                {props.hestory.history.location.pathname}
-              </span>{" "}
-              was not found.
-            </h3>
+        <div className="error-container">
+          <div className="row">
+            <div className="col s12 center">
+              <h1>Error 404</h1>
+              <h2>page not found</h2>
+              <h3>
+                The requested URL :
+                <span className="notfound">
+                  {props.hestory.history.location.pathname}
+                </span>{" "}
+                was not found.
+              </h3>
+            </div>
           </div>
         </div>
       </div>
