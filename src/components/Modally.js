42f0279834f1a364ac32575390e9b34e9aa9de88
Peter Ihimire <peterihimire@gmail.com>
2020-12-07 14:55:35 +0100

    Still working on the modal

--- src/components/Modally.js
+++ src/components/Modally.js
@@ -13,7 +13,7 @@ class Modally extends Component {
   render() {
     return (
       <>
-        <div className="modal-section">
+        {/* <div className="modal-section">
           <div className="container">
             <div className="modal-btn-div">
               <a href="#get" className=" modal-trigger  btn btn-bigger ">
@@ -64,6 +64,96 @@ class Modally extends Component {
                           required
                         />
                         <label for="password">Password</label>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> */}
+
+        {/* <div className="modal-section">
+          <div className="container">
+            <div className="modal-btn-div">
+              <a href="#get" className=" modal-trigger  btn btn-bigger ">
+                modal btn
+              </a>
+            </div>
+          </div>
+        </div> */}
+
+        <div id="get" className="modal2">
+          <div className="modal-content">
+            <div className="row">
+              <div className="container black-txt">
+                <div className="col s12 m5">
+                  <div className="modal-bg-div">
+                    <a href="/" className="hidden-xs">
+                      <img
+                        src={logoAlt}
+                        alt="Paymonthly Cars"
+                        className="modal-logo"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="col s12 m7 rel m-c">
+                  <div className="close-div right-align-xs rel">
+                    <a href="/" class="visible-xs">
+                      <img
+                        src="img/paymonthlycars-1.svg"
+                        alt="Paymonthly Cars"
+                        className="modal-logo"
+                      />
+                    </a>
+                    <img
+                      src="img/back-icon.svg"
+                      alt="close modal"
+                      className="modal-close cursor"
+                    />
+                  </div>
+                  <form action="https://paymonthlycars.com/login" method="POST">
+                    <input
+                      type="hidden"
+                      name="_token"
+                      value="HpHEtnrja8LWLNitDIz0ir0kKFDsgBpx68UPxcXk"
+                    />
+                    <h4 className="modal-title sec-title">Login</h4>
+                    <div className="row">
+                      <div className="col s12">
+                        <div className="input-field col s12 m10 l7 pad0">
+                          <input
+                            type="email"
+                            id="email"
+                            value=""
+                            name="email"
+                            required
+                          />
+                          <label for="email">Email</label>
+                        </div>
+                      </div>
+                      <div className="col s12">
+                        <div className="input-field col s12 m10 l7 pad0">
+                          <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value=""
+                            required
+                          />
+                          <label for="password">Password</label>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="row mgd">
+                      <div className="col s12">
+                        <div>
+                          <button type="submit" className="submit-btn">
+                            Login
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </form>
