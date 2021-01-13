a7381d5237065fb7147abf5bb77724ea641f19dd
Peter Ihimire <peterihimire@gmail.com>
2021-01-13 19:44:54 +0100

    Added the banner-img on mobile phone display, for the about component, adjusted the heading hidden in the navbar

--- src/components/About.js
+++ src/components/About.js
@@ -9,39 +9,35 @@ const HomeAbout = () => {
         <div className="row">
           <div className="col s12">
             <div className="col s12 m7 l5">
-              <div className="hxs">
-                <div className="about-text-container">
-                  <div className="about-header">
-                    <h3>Our Vision</h3>
+              <div className="about-text-container">
+                <div className="about-header">
+                  <h3>Our Vision</h3>
+                </div>
+                <div className="visible-xs">
+                  <div className="about-image-container">
+                    <img src={pix} alt="person" width="100%" />
                   </div>
-                  <div className="visible-xs">
-                    <div className="about-image-container">
-                      <img src={pix} alt="person" width="100%" />
-                    </div>
-                  </div>
-                  <div className="about-text-content">
-                    <p className="">
-                      I had the best experience shopping with vasiti. Usability
-                      of the website was great, very good customer service, an
-                      all round great experience. I would definately be coming
-                      back! I had the best experience shopping with vasiti.
-                      Usability of the website was great, very good customer
-                      service, an all round great experience. I would definately
-                      be coming back!
-                    </p>
-                    <p>
-                      I had the best experience shopping with vasiti. Usability
-                      of the website was great, very good customer service, an
-                      all round great experience. I would definately be coming
-                      back! I had the best experience shopping with vasiti.
-                      Usability of the website was great, very good customer
-                      service, an all round great experience. I would definately
-                      be coming back! I had the best experience shopping with
-                      vasiti. Usability of the website was great, very good
-                      customer service, an all round great experience. I would
-                      definately be coming back!
-                    </p>
-                  </div>
+                </div>
+                <div className="about-text-content">
+                  <p className="">
+                    I had the best experience shopping with vasiti. Usability of
+                    the website was great, very good customer service, an all
+                    round great experience. I would definately be coming back! I
+                    had the best experience shopping with vasiti. Usability of
+                    the website was great, very good customer service, an all
+                    round great experience. I would definately be coming back!
+                  </p>
+                  <p>
+                    I had the best experience shopping with vasiti. Usability of
+                    the website was great, very good customer service, an all
+                    round great experience. I would definately be coming back! I
+                    had the best experience shopping with vasiti. Usability of
+                    the website was great, very good customer service, an all
+                    round great experience. I would definately be coming back! I
+                    had the best experience shopping with vasiti. Usability of
+                    the website was great, very good customer service, an all
+                    round great experience. I would definately be coming back!
+                  </p>
                 </div>
               </div>
             </div>
